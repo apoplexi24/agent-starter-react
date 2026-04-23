aa1196cc5764723cf0b42f2c2127fa8ba151a195
apoplexi24 <shivanandanasharma@gmail.com>
2026-04-23 12:51:00 +0530

    feat: added orders bot option

--- components/app/welcome-view.tsx
+++ components/app/welcome-view.tsx
@@ -88,6 +88,7 @@ export const WelcomeView = ({
               <option value="trial_agent">KYC Bot</option>
               <option value="payments_trial_bot">Payment Bot</option>
               <option value="mf_trial_bot">MF Bot</option>
+              <option value="orders_trial_bot">Orders Bot</option>
             </select>
           </div>
           <div className="text-left">
